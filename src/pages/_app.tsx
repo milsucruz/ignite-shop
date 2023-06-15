b29e5b14e0905faaead70fae3e340d6f27adc445
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-15 14:02:43 -0300

    Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
+import logo from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logo.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+
+  )
 }

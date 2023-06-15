349fff2c351fdaebb09d8cf492fe51caad460a7f
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-15 15:06:01 -0300

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
+import { HomeContainer, Product } from "../styles/pages/home";
+import Image from 'next/image'
+
+import shirt1 from '../assets/shirts/1.png'
+import shirt2 from '../assets/shirts/2.png'
+import shirt3 from '../assets/shirts/3.png'
+import shirt4 from '../assets/shirts/4.png'
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
-  )
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+    )
 }

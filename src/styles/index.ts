f340132f41eb475ca9a17787d36e1ea5f2eb6231
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-15 10:59:22 -0300

    Theme colors

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -11,6 +11,16 @@ export const {
   createTheme
  } = createStitches({
   theme: {
+    colors: {
+      white: '#FFF',
 
+      gray900: '#121214',
+      gray800: '#202024',
+      grya300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
+    }
   }
 })

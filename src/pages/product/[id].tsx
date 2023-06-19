f8e3df1bc0904b440dc3ac3daebe3c4850e215c6
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-19 17:13:28 -0300

    Products page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,21 @@
-import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/products'
 
 export default function Porduct() {
-  const { query } = useRouter()
 
   return(
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camisa X</h1>
+        <span>R$ 79,00</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique odio fugiat expedita molestias enim inventore totam provident alias illum iure rem illo ab, quam architecto nostrum maiores distinctio, delectus voluptatum.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }

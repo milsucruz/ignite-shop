d3e1fe12e95759cc4208bbd7464a58a5112c6f7a
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-19 16:57:05 -0300

    Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Image from 'next/image'
+import Link from "next/link";
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -32,13 +33,15 @@ export default function Home({products}: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider" >
       {products.map(product => {
         return(
-          <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} alt="" width={520} height={480}/>
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+          <Link key={product.id} href={`/product/${product.id}`} >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} alt="" width={520} height={480}/>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
